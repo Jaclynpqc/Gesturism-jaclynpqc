90d10d24d58f5a3923d1ffd354b0ce3fc7a0e158
Jaclyn Pham <pham02@allegheny.edu>
2024-09-24 23:15:22 -0400

    some demo

--- demo/HandTrackingModule.py
+++ demo/HandTrackingModule.py
@@ -13,7 +13,7 @@ class handDetector():
         self.hands = self.mpHands.Hands(self.mode, self.maxHands,
                                         self.detectionCon, self.trackCon) #default max detection 2 hands
         self.mpDraw = mp.solutions.drawing_utils
-
+        self.tipIds = [4,8,12,16,20]
     def findHands(self, img, draw = True):
         imgRGB = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
         self.results = self.hands.process(imgRGB)
@@ -29,17 +29,34 @@ class handDetector():
         return img
     
     def findPosition(self, img, handNo = 0, draw = True):
-        lmList = []
+        self.lmList = []
         if self.results.multi_hand_landmarks:
             myHand = self.results.multi_hand_landmarks[handNo]
             for id,lm in enumerate(myHand.landmark):
                                 h,w,c = img.shape #check out the height, weight and channel
                                 cx,cy = int(lm.x*w),int(lm.y*h) # find the position of the center
-                                lmList.append([id,cx,cy])
+                                self.lmList.append([id,cx,cy])
                                 if draw:
                                     cv2.circle(img,(cx,cy),15,(255,0,255),cv2.FILLED) #detecting the 0 poimt landmark
         
-        return lmList
+        return self.lmList
+    
+    def fingersUp(self):
+        fingers = []
+        #Thumb
+        if self.lmList[self.tipIds[0]][1] < self.lmList[self.tipIds[0]-1][1]:
+            fingers.append(1)
+        else:
+            fingers.append(0)
+
+        # 4 Fingers
+        for id in range (1,5):
+            if self.lmList[self.tipIds[id]][2] < self.lmList[self.tipIds[id]-2][2]:
+                fingers.append(1)
+            else:
+                fingers.append(0)
+        return fingers
+        
                         
 def main():
     pTime = 0
--- demo/VirtualPainter.py
+++ demo/VirtualPainter.py
@@ -4,6 +4,11 @@ import time
 import os
 import HandTrackingModule as html
 
+
+#################
+brushThickness = 15
+eraserThickness = 50
+#################
 #path to header folder
 folderPath = "Header"
 myList = os.listdir(folderPath)
@@ -15,16 +20,78 @@ for imPath in myList:
 print(len(overLayList))
 
 
-header = overLayList[1]
+header = overLayList[5]
+drawColor = (245,245,245)
+# FIX: No idea why the loop does this, but the order is 456231
 #Create loop to run webcam
 
 cap = cv2.VideoCapture(1)
 cap.set(3, 1280)
 cap.set(4, 720)
 
-while True:
-    success, img = cap.read()
+detector = html.handDetector(detectionCon=1)
+xp, yp =0,0
+imgCanvas = np.zeros((720,1280,3), np.uint8)
 
+while True:
+    # 1. Import image
+    success, img = cap.read()
+    img = cv2.flip(img,1) #flip image horizontally
+
+    # 2. Find Hand Landmarks
+    img = detector.findHands(img)
+    lmList = detector.findPosition(img, draw=False)
+
+    if len(lmList) != 0:
+        #print(lmList)
+        # Get the tip of the index and middle fingers
+        x1,y1 = lmList[8][1:] #except id number
+        x2,y2 = lmList[12][1:]
+
+        #3. Check which fingers are up
+        fingers = detector.fingersUp()
+        #print(fingers)
+    
+        # 4. If Selection mode- TWO fingers are up
+        if fingers[1] and fingers[2]:
+            cv2.rectangle(img, (x1,y1-25), (x2,y2+25), drawColor, cv2.FILLED)
+            print("Selection Mode")
+            #Checking for the click
+            if y1< 125:
+                if 200<x1<350:
+                    header = overLayList[3]
+                    drawColor = (255,182,193) #pink
+                elif 400<x1<550:
+                    header = overLayList[4]
+                    drawColor = (255,165,0) #orange
+                elif 600<x1<750:
+                    header = overLayList[0]
+                    drawColor = (255,255,0) #yellow
+                elif 800<x1<950:
+                    header = overLayList[1]
+                    drawColor = (50,205,50) #green
+                elif 1000<x1<1150:
+                    header = overLayList[2]
+                    drawColor = (0,0,0) #erase = black
+        # 5. If Drawing mode- Index finger is up
+        if fingers[1] and fingers[2]==False:
+            cv2.circle(img, (x1,y1),15,drawColor, cv2.FILLED)
+            print("Drawing Mode")
+            if xp == 0 and yp == 0:
+                xp, yp = x1, y1
+
+            if drawColor == (0,0,0):
+                cv2.line(img, (xp,yp), (x1,y1),drawColor, eraserThickness)
+                cv2.line(imgCanvas, (xp,yp), (x1,y1),drawColor, eraserThickness)
+
+            else:
+                cv2.line(img, (xp,yp), (x1,y1),drawColor, brushThickness)
+                cv2.line(imgCanvas, (xp,yp), (x1,y1),drawColor, brushThickness)
+            xp,yp = x1, y1
+
+    # Setting the header image
     img[0:125, 0:1280] = header #carve space for header
+    img = cv2.addWeighted(img,0.5,imgCanvas,0.5,0)
     cv2.imshow("Image", img)
+    #cv2.imshow("Canvas", imgCanvas)
     cv2.waitKey(1)
